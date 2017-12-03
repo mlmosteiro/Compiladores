e6a131a60f6ffce9a75628e3f4cec1c3f5914082
Maria Luz Mosteiro <marymost22@gmail.com>
2017-12-03 01:44:26 +0100

    Actualizados los mensajes de error

--- Practica3/ErrorManager.c
+++ Practica3/ErrorManager.c
@@ -2,23 +2,41 @@
 #include "ErrorManager.h"
 
 
+#define MISSING_ARGUMENTS 			(-101)
+#define NOT_FILE_FOUNDED 			(-103)
+#define NOT_INITIALIZED_VARIABLE	(-104)
+#define OVERWITE_VARIABLE 			(-105)
+#define ILLEGAL_CHARACTER 			(-107)
+#define NOT_FUNCTION	 			(-108)
+#define DIVISION_BY_ZERO			(-109)
+#define VALUE_OF_FUNCTION			(-110)
+
+
 void showError(int errorCode, int numLinea) {
     printf("\033[0;31m");
     switch (errorCode) {
-        case MISSING_ARGUMENTS:
-            printf("Número de argumentos insuficiente.\nSintaxis: ./practica1 <archivoCodigoFuente> <archivoDefiniciones>");
-            break;
-        case NOT_KEYWORDS_FILE_FOUNDED:
-            printf("No se ha encontrado el archivo de palabras clave especificado\n");
-            break;
         case NOT_FILE_FOUNDED:
             printf("No se ha encontrado el archivo de código fuente especificado\n");
             break;
-       case ILLEGAL_ESCAPED_CHARACTER:
-            printf("[ERROR LEXICO - linea %d] \tCaracter escapado ilegal.\n", numLinea);
+
+        case VALUE_OF_FUNCTION:
+            printf("[ERROR] No se puede mostrar el valor de una funcion\n");
+            break;
+
+        case NOT_INITIALIZED_VARIABLE:
+            printf("[ERROR] Esa variable no está inicializada\n");
+            break;
+        case OVERWITE_VARIABLE:
+            printf("[ERROR] Se está intentando sobreescribir una constante.\n");
+            break;
+        case NOT_FUNCTION:
+            printf("[ERROR] Función no existente\n");
             break;
         case ILLEGAL_CHARACTER:
-            printf("[ERROR LEXICO - linea %d] \tCaracter ilegal.\n", numLinea);
+            printf("[ERROR LEXICO - linea %d] Carácter ilegal.\n", numLinea);
+            break;
+        case DIVISION_BY_ZERO:
+            printf("[ERROR] División por cero\n");
             break;
         default:
             printf("[ERROR]");
